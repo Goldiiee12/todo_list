96e10d284eae8a0769aa81d048075c4a5c25b601
Caleb <104283846+Goldiiee12@users.noreply.github.com>
2024-02-17 23:47:16 -0700

    Final edit and completion of todo-list

--- src/js/dateUtils.js
+++
@@ -1,5 +0,0 @@
-// src/js/dateUtils.js
-export function formatDate(date) {
-    return date.toISOString().split('T')[0];
-  }
-  
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,13 @@
-// /webpack.config.js
-
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Todo List',
-      template: 'src/index.html' // You can specify a template file here
-    }),
-    new MiniCssExtractPlugin(), // Add this line to instantiate the plugin
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  devServer: {
-    static: './dist',
-  },
-  devtool: 'inline-source-map',
 };
+
 
 
 
